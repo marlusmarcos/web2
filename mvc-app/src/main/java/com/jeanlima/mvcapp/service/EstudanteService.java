648dc12bc4c893e849c7f568912fc4d63ffa236b
Marlus Marcos <marlus.marcos.018@ufrn.edu.br>
2022-09-09 15:51:03 -0300

    projetos de web2 com mini projeto 1

---
+++ mvc-app/src/main/java/com/jeanlima/mvcapp/service/EstudanteService.java
@@ -0,0 +1,24 @@
+package com.jeanlima.mvcapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jeanlima.mvcapp.model.Estudante;
+
+@Service
+public interface EstudanteService {
+
+
+
+    public void salvarEstudante(Estudante estudante);
+    public void deletarEstudante(Estudante estudante);
+    public Estudante getEstudanteById(Integer id);
+    public List <Estudante> getEstudanteByCurso (String curso);
+
+
+
+    
+    
+    
+}
---
+++ mvc-app/src/main/java/com/jeanlima/mvcapp/service/EstudanteServiceImpl.java
@@ -0,0 +1,51 @@
+package com.jeanlima.mvcapp.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.jeanlima.mvcapp.model.Estudante;
+
+@Component
+public class EstudanteServiceImpl implements EstudanteService{
+
+    private List<Estudante> estudantes;
+
+    public List<Estudante> getEstudantes() {
+        return estudantes;
+    }
+
+    @Override
+    public void salvarEstudante(Estudante estudante) {
+        this.estudantes.add(estudante);
+        
+    }
+
+    @Override
+    public void deletarEstudante(Estudante estudante) {
+        
+        this.estudantes.remove(estudante);
+        
+    }
+
+    @Override
+    public Estudante getEstudanteById(Integer id) {
+        for (Estudante estudante : estudantes) {
+            if(estudante.getId() == id){
+                return estudante;
+            }
+        }
+        return null;
+    }
+
+	@Override
+	public List<Estudante> getEstudanteByCurso(String curso) {
+		List <Estudante> estudantesTemp = null;
+		for (Estudante e : estudantes) {
+			if (e.getCurso() == curso) estudantesTemp.add(e);
+		}
+
+		return estudantesTemp;
+	}
+    
+}
