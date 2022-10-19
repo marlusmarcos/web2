4474b1598eb8860183a58c2db6ca29ea918edbd7
marlusmarcos <marlusufrn@gmail.com>
2022-10-19 14:54:33 -0300

    patch produto

--- spring-rest-api/src/main/java/com/jeanlima/springrestapi/rest/controllers/ProdutoController.java
+++ spring-rest-api/src/main/java/com/jeanlima/springrestapi/rest/controllers/ProdutoController.java
@@ -3,15 +3,19 @@ package com.jeanlima.springrestapi.rest.controllers;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.jeanlima.springrestapi.model.Cliente;
 import com.jeanlima.springrestapi.model.Produto;
 import com.jeanlima.springrestapi.repository.ProdutoRepository;
 
@@ -85,5 +90,23 @@ public class ProdutoController {
 
         Example example = Example.of(filtro, matcher);
         return repository.findAll(example);
+    }
+    @PatchMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarParcial( @PathVariable Integer id,
+                        @RequestBody Map<String, Object> campos ){
+    	repository
+                .findById(id)
+                .map( produto -> {
+                    campos.forEach((nomePropriedade, valorPropriedade) -> {
+                    	Field field = ReflectionUtils.findField(Produto.class, nomePropriedade);
+                    	field.setAccessible(true);
+                    	ReflectionUtils.setField(field, produto, valorPropriedade);
+                    	System.out.println("nome propriedade: " + nomePropriedade + "*** valor da propriedade: " + valorPropriedade);
+                    });
+                    repository.save(produto);
+                    return produto;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Cliente não encontrado") );
     }
 }
